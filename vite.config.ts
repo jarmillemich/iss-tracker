36cf2e3c0e96d91662a2935ed0068af9d3d34176
Jared Miller <jarmille@umich.edu>
2022-09-26 17:18:45 -0400

    Fixed base path for GH pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Path on GH pages
+  base: '/iss-tracker/',
   plugins: [vue()],
   resolve: {
     alias: {
